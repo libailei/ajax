08e506b39f1d82348dd7e9ecce83c3b7ad4c2ec8
houlei <nunu18941198157@gmail.com>
2018-05-10 22:12:15 +0800

    Ajax

---
+++ Ajax.js
@@ -0,0 +1,63 @@
+GET
+// 创建 AJAX 对象
+var r = new XMLHttpRequest()
+// 设置请求方法和请求地址
+r.open('GET', '/login', true)
+// 注册响应函数
+r.onreadystatechange = function() {
+    console.log('state change', r)
+}
+// 发送请求
+r.send()
+
+
+POST
+// 创建 AJAX 对象
+var r = new XMLHttpRequest()
+// 设置请求方法和请求地址
+r.open('POST', '/login', true)
+// 设置发送的数据的格式
+r.setRequestHeader('Content-Type', 'application/json')
+// 注册响应函数
+r.onreadystatechange = function() {
+    if (r.readyState === 4) {
+        console.log('state change', r, r.status, r.response)
+        var response = JSON.parse(r.response)
+        console.log('response', response)
+    } else {
+        console.log('change')
+    }
+}
+// 发送请求
+var account = {
+    username: 'gua',
+    password: '123',
+}
+var data = JSON.stringify(account)
+r.send(data)
+
+GET + POST 
+var ajax = function(request) {
+    /*
+    request 是一个 object, 有如下属性
+        method, 请求的方法, string
+        url, 请求的路径, string
+        data, 请求发送的数据, 如果是 GET 方法则没这个值, string
+        callback, 响应回调, function
+    */
+    var r = new XMLHttpRequest()
+    r.open(request.method, request.url, true)
+    if (request.contentType !== undefined) {
+        r.setRequestHeader('Content-Type', request.contentType)
+    }
+    r.onreadystatechange = function(event) {
+        if(r.readyState === 4) {
+            request.callback(r.response)
+        }
+    }
+    if (request.method === 'GET') {
+        r.send()
+    } else {
+        r.send(request.data)
+    }
+}
